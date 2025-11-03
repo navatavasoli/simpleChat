a03f2d89c219d77d714ef93e0477c298928e56cb
Nava <navatavasoli8@gmail.com>
2025-11-03 13:39:46 -0500

    Tested repository connection
    I added a comment to test if the repo. was connected properly and could recognize file changes.

--- edu/seg2105/edu/server/backend/EchoServer.java
+++ edu/seg2105/edu/server/backend/EchoServer.java
@@ -2,6 +2,7 @@ package edu.seg2105.edu.server.backend;
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at www.lloseng.com 
+// editing test
 
 
 import ocsf.server.*;
