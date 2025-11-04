6a7815e2d1426aaf06e7964e65dc0b74782441ce
Nava <navatavasoli8@gmail.com>
2025-11-03 21:57:31 -0500

    Created ServerConsole.java file

---
+++ edu/seg2105/edu/server/backend/ServerConsole.java
@@ -0,0 +1,18 @@
+package edu.seg2105.edu.server.backend;
+import edu.seg2105.client.common.ChatIF;
+
+public class ServerConsole implements ChatIF {
+	
+	private EchoServer server;
+	
+	public ServerConsole(EchoServer server) {
+		this.server = server;
+	}
+
+	@Override
+	public void display(String message) {
+		System.out.println("SERVER MSG>" + message);
+		server.sendToAllClients("SERVER MSG>" + message);
+	}
+
+}
