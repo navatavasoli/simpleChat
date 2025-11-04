9684a0e059f57f98c43caf245e64f4bf57cc40d7
Nava <navatavasoli8@gmail.com>
2025-11-03 22:56:35 -0500

    Update ServerConsole.java
    Setup all the comments I need for the implementations

--- edu/seg2105/edu/server/backend/ServerConsole.java
+++ edu/seg2105/edu/server/backend/ServerConsole.java
@@ -14,5 +14,30 @@ public class ServerConsole implements ChatIF {
 		System.out.println("SERVER MSG>" + message);
 		server.sendToAllClients("SERVER MSG>" + message);
 	}
+	
+	  /**
+	   * Handles commands from the user. 
+	   * @param command the command prompt the user enters into the console.
+	   */
+	  private void handleCommand(String command) {
+		  if(command.equals("#quit")) {
+			  // quit gracefully
+		  } else if(command.equals("#stop")) {
+			  // stop listening for new clients
+			  stopListening();
+		  } else if(command.equals("#close")) {
+			  // stop listening for new clients and disconnect existing clients
+		  } else if(command.equals("#setport <port>")) {
+			// if server is closed, stop listening for new clients
+			  
+		  } else if(command.equals("#start")) {
+			  // server starts listening for new clients if server is stopped
+			  
+		  } else if(command.equals("#getport")) {
+			  // display the current port number 
+			  
+		  }
+	  
+	  }
 }
 
